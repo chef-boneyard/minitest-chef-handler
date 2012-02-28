a1a3e23b581d99671f6b3d6e24dd1a368e7614f9
David Calavera <david.calavera@gmail.com>
2012-02-28 15:07:31 -0800

    fix solo example

--- examples/solo.rb
+++ examples/solo.rb
@@ -1,4 +1,4 @@
 require 'minitest-chef-handler'
 
-report_handlers << MiniTest::Chef::TestHandler.new
+report_handlers << MiniTest::Chef::Handler.new
 cookbook_path File.expand_path('cookbooks', File.dirname(__FILE__))
