3865d6328c8a7d38f90eacddd64f17e21b573352
Steven Wagner <leftathome@gmail.com>
2012-07-11 16:25:04 -0700

    Changed CI_Runner to CIRunner.  Also removed extraneous "require" ...

--- lib/minitest-chef-handler/ci_runner.rb
+++ lib/minitest-chef-handler/ci_runner.rb
@@ -1,7 +1,6 @@
-require 'ci/reporter/minitest'
 module MiniTest
   module Chef
-    class CI_Runner < CI::Reporter::Runner
+    class CIRunner < CI::Reporter::Runner
       attr_reader :run_status
 
       def initialize(run_status)
